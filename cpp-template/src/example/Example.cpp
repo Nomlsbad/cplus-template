60c4a82f82b1c1a36c4855cc957a1b9f8450dbb6
Ilia Yurasov <iliayurasov@yandex.ru>
2024-06-14 02:07:48 +0300

    Add main project folder

---
+++ cpp-template/src/example/Example.cpp
@@ -0,0 +1,13 @@
+#include "Example.hpp"
+
+#include <iostream>
+
+void Example::sayHello()
+{
+    std::cout << "Hello!";
+}
+
+int Example::sum(int a, int b)
+{
+    return a + b;
+}
---
+++ cpp-template/src/example/Example.hpp
@@ -0,0 +1,13 @@
+#ifndef CPP_TEMPLATE_EXAMPLE_HPP
+#define CPP_TEMPLATE_EXAMPLE_HPP
+
+class Example
+{
+public:
+
+    static void sayHello();
+    static int sum(int a, int b);
+};
+
+
+#endif // CPP_TEMPLATE_EXAMPLE_HPP
